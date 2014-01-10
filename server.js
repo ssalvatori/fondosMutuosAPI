ab2876c0e785967f689447fabbd5caaa011a6f48
Stefano Salvatori <ssalvatori@gmail.com>
2014-01-09 22:36:51 -0300

    added verbose mongoose

--- server.js
+++ server.js
@@ -49,6 +49,18 @@ server.use(function authenticate(req, res, next) {
     next();
 });
 
+mongoose.connection.on('connected', function() {
+    console.log('Mongoose connection open');
+});
+
+mongoose.connection.on('error', function(err) {
+    console.log('Mongoose connection error: ' + err);
+});
+
+mongoose.connection.on('disconnected', function() {
+    console.log('Mongoose connection disconnected');
+});
+
 mongoose.connect(mongoURI, {server:{auto_reconnect:true}} ,function (err, res) {
     'use strict';
 	if (err) {
